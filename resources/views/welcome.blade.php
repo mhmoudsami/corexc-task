32ac705eb02026034a46312fe4e8a51e10f089fd
Mahmoud Sami <mahmodsaamy@gmail.com>
2020-11-03 17:02:58 +0200

    moved static layout into laravel app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,235 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-        <title>Laravel</title>
-    </head>
-    <body class="antialiased">
-        
-    </body>
+    <meta http-equiv="pragma" content="no-cache">
+    <meta http-equiv="cache-control" content="max-age=604800">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Corexc Task</title>
+
+
+    <!-- CSS files-->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" type="text/css">
+
+
+    <!-- custom style -->
+    <link href="./css/ui.css" rel="stylesheet" type="text/css">
+    <link href="./css/responsive.css" rel="stylesheet" media="only screen and (max-width: 1200px)">
+</head>
+<body>
+
+	<header class="section-header">
+	    <section class="header-main border-bottom">
+	        <div class="container">
+	            <div class="row align-items-center">
+	                <div class="col-lg-2 col-4">
+	                    <a href="/" class="brand-wrap">
+	                        Corexc.com
+	                    </a> <!-- brand-wrap.// -->
+	                </div>
+	                <div class="col-lg-6 col-sm-12">
+	                    
+	                </div> <!-- col.// -->
+	                <div class="col-lg-4 col-sm-6 col-12">
+	                    <div class="widgets-wrap float-md-right">
+	                        <div class="widget-header  mr-3">
+	                            <a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-shopping-cart"></i></a>
+	                            <span class="badge badge-pill badge-danger notify">0</span>
+	                        </div>
+	                    </div> <!-- widgets-wrap.// -->
+	                </div> <!-- col.// -->
+	            </div> <!-- row.// -->
+	        </div> <!-- container.// -->
+	    </section> <!-- header-main .// -->
+	</header> <!-- section-header.// -->
+
+
+	<!-- ========================= SECTION PAGETOP ========================= -->
+	<section class="section-pagetop bg">
+	    <div class="container">
+	        <h2 class="title-page">Search Results</h2>
+	</section>
+
+
+	<section class="section-content padding-y">
+	    <div class="container">
+	        <div class="row">
+	            <aside class="col-md-3">
+	                        
+	                <div class="card">
+	                    <article class="filter-group">
+	                        <header class="card-header">
+	                            <a href="#" data-toggle="collapse" data-target="#collapse_1" aria-expanded="true" class="">
+	                                <i class="icon-control fa fa-chevron-down"></i>
+	                                <h6 class="title">Category</h6>
+	                            </a>
+	                        </header>
+	                        <div class="filter-content collapse show" id="collapse_1" style="">
+	                            <div class="card-body">
+	                                <ul class="list-menu">
+	                                    <li><a href="#">People  </a></li>
+	                                    <li><a href="#">Watches </a></li>
+	                                    <li><a href="#">Cinema  </a></li>
+	                                    <li><a href="#">Clothes  </a></li>
+	                                    <li><a href="#">Home items </a></li>
+	                                    <li><a href="#">Animals</a></li>
+	                                    <li><a href="#">People </a></li>
+	                                </ul>
+	                            </div> <!-- card-body.// -->
+	                        </div>
+	                    </article> <!-- filter-group  .// -->
+	                    <article class="filter-group">
+	                        <header class="card-header">
+	                            <a href="#" data-toggle="collapse" data-target="#collapse_2" aria-expanded="true" class="">
+	                                <i class="icon-control fa fa-chevron-down"></i>
+	                                <h6 class="title">Brands </h6>
+	                            </a>
+	                        </header>
+	                        <div class="filter-content collapse show" id="collapse_2" style="">
+	                            <div class="card-body">
+	                                <label class="custom-control custom-checkbox">
+	                                  <input type="checkbox" checked="" class="custom-control-input">
+	                                  <div class="custom-control-label">Mercedes  
+	                                    <b class="badge badge-pill badge-light float-right">120</b>  </div>
+	                                </label>
+	                                <label class="custom-control custom-checkbox">
+	                                  <input type="checkbox" checked="" class="custom-control-input">
+	                                  <div class="custom-control-label">Toyota 
+	                                    <b class="badge badge-pill badge-light float-right">15</b>  </div>
+	                                </label>
+	                                <label class="custom-control custom-checkbox">
+	                                  <input type="checkbox" checked="" class="custom-control-input">
+	                                  <div class="custom-control-label">Mitsubishi 
+	                                    <b class="badge badge-pill badge-light float-right">35</b> </div>
+	                                </label>
+	                                <label class="custom-control custom-checkbox">
+	                                  <input type="checkbox" checked="" class="custom-control-input">
+	                                  <div class="custom-control-label">Nissan 
+	                                    <b class="badge badge-pill badge-light float-right">89</b> </div>
+	                                </label>
+	                                <label class="custom-control custom-checkbox">
+	                                  <input type="checkbox" class="custom-control-input">
+	                                  <div class="custom-control-label">Honda 
+	                                    <b class="badge badge-pill badge-light float-right">30</b>  </div>
+	                                </label>
+	                    		</div> <!-- card-body.// -->
+	                        </div>
+	                    </article> <!-- filter-group .// -->
+
+	                    <article class="filter-group">
+	                        <header class="card-header">
+	                            <a href="#" data-toggle="collapse" data-target="#collapse_3" aria-expanded="true" class="">
+	                                <i class="icon-control fa fa-chevron-down"></i>
+	                                <h6 class="title">Price range </h6>
+	                            </a>
+	                        </header>
+	                        <div class="filter-content collapse show" id="collapse_3" style="">
+	                            <div class="card-body">
+	                                <input type="range" class="custom-range" min="0" max="100" name="">
+	                                <div class="form-row">
+	                                <div class="form-group col-md-6">
+	                                  <label>Min</label>
+	                                  <input class="form-control" placeholder="$0" type="number">
+	                                </div>
+	                                <div class="form-group text-right col-md-6">
+	                                  <label>Max</label>
+	                                  <input class="form-control" placeholder="$1,0000" type="number">
+	                                </div>
+	                                </div> <!-- form-row.// -->
+	                                <button class="btn btn-block btn-primary">Apply</button>
+	                            </div><!-- card-body.// -->
+	                        </div>
+	                    </article> <!-- filter-group .// -->
+	                </div> <!-- card.// -->
+
+	            </aside> <!-- col.// -->
+
+	            
+	            <main class="col-md-9">
+
+	                <header class="border-bottom mb-4 pb-3">
+	                    <div class="form-inline">
+	                        <span class="mr-md-auto">32 Items found </span>
+	                        <select class="mr-2 form-control">
+	                            <option>Latest items</option>
+	                            <option>Trending</option>
+	                            <option>Most Popular</option>
+	                            <option>Cheapest</option>
+	                        </select>
+	                    </div>
+	                </header><!-- sect-heading -->
+
+
+	                <article class="card card-product-list">
+	                    <div class="row no-gutters">
+	                        <aside class="col-md-3">
+	                            <a href="#" class="img-wrap">
+	                                <img src="./images/3.jpg">
+	                            </a>
+	                        </aside> <!-- col.// -->
+	                        <div class="col-md-6">
+	                            <div class="info-main">
+	                                <a href="#" class="h5 title"> Great product name goes here  </a>
+	                                
+	                                <p> Take it as demo specs, ipsum dolor sit amet, consectetuer adipiscing elit, Lorem ipsum dolor sit amet, consectetuer adipiscing elit, Ut wisi enim ad minim veniam </p>
+	                            </div> <!-- info-main.// -->
+	                        </div> <!-- col.// -->
+	                        <aside class="col-sm-3">
+	                            <div class="info-aside">
+	                                <div class="price-wrap">
+	                                    <span class="price h5"> $140 </span>    
+	                                </div> <!-- info-price-detail // -->
+	                                <br>
+
+	                                    <div class="input-group input-spinner">
+	                                        <div class="input-group-prepend">
+	                                            <button class="btn btn-light" type="button" id="button-minus"> <i class="fa fa-minus"></i> </button>
+	                                        </div>
+	                                        <input type="text" class="form-control" value="1">
+	                                        <div class="input-group-append">
+	                                            <button class="btn btn-light" type="button" id="button-plus"> <i class="fa fa-plus"></i> </button>
+	                                        </div>
+	                                    </div>
+	                                    <br>
+	                                    <p style="margin-top: 5px;">
+	                                        <a href="#" class="btn btn-outline-primary btn-sm"> 
+	                                            Add to cart 
+	                                            <i class="fa fa-shopping-cart"></i> 
+	                                        </a>
+	                                    </p>
+	                            </div> <!-- info-aside.// -->
+	                        </aside> <!-- col.// -->
+	                    </div> <!-- row.// -->
+	                </article> <!-- card-product .// -->
+
+
+
+	                <nav aria-label="Page navigation sample">
+	                    <ul class="pagination">
+	                        <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
+	                        <li class="page-item active"><a class="page-link" href="#">1</a></li>
+	                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+	                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+	                        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+	                    </ul>
+	                </nav>
+
+	            </main> <!-- col.// -->
+	        </div>
+	    </div> <!-- container .//  -->
+	</section>
+
+	<footer class="section-footer border-top padding-y">
+	    <div class="container">
+	        <p class="float-md-right"> 
+	            © Copyright 2019 All rights reserved
+	        </p>
+	    </div><!-- //container -->
+	</footer>
+
+	<script src="/js/app.js"></script>
+</body>
 </html>
