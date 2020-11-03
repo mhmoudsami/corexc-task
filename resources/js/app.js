e55a122ef87b6be2f779c3388e5bda942aa7fa11
Mahmoud Sami <mahmodsaamy@gmail.com>
2020-11-03 17:15:30 +0200

    initial commit for vue app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
+import Vue from 'vue';
+import App from './components/App';
+
 require('./bootstrap');
+
+
+
+new Vue({
+    el: '#main-app',
+    render: h => h(App),
+});
---
+++ resources/js/components/Temp.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            source: String,
+        },
+        data: () => ({
+        }),
+        watch: {
+        },
+        mounted(){
+        },
+        methods: {
+        }
+    }
+</script>
